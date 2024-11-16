648488a10b6b3ad2dc6e978c0d1e5fb79f1db204
Karolina Sz <128708615+KarlaSz@users.noreply.github.com>
2024-11-16 19:06:20 +0100

    Update encrypted-word.py

--- encrypted-word.py
+++ encrypted-word.py
@@ -1,7 +1,7 @@
 #szyfrowanie slow wg szyfru cezara 
 
-#ord przyjmuje liste, cyfre lub inny dowolny znak i zwraca przypisana liczbe wg tablicy ascii
-     #chr zmienia spowrotem liczbe na znak
+     #ord przyjmuje liste, cyfre lub inny dowolny znak i zwraca przypisana liczbe wg tablicy ascii
+     #chr zmienia spowrotem liczbe na znak, odwrotnosc ord
      # jesli przekroczy ostatni znak w alfabecie (jest icg 26) to wtedy ma cofnac sie na poczatek zeby nie bylo znakow spejclanych
      # key to informacja o ile liter przesuwamy sie w alfabecie
      #algorytm poprawnie tez szyfruje spacje i inne znaki spoza alfabetu, wiec 1 if przepisuje nierozpoznane znaki
@@ -41,3 +41,17 @@ def deencrypt(message, key):
      
      
 print(deencrypt('ZdnrDrzdqh', 3))
+
+
+
+###
+
+print(ord('a'))  # Wynik: 97 (kod ASCII dla 'a')
+print(ord('A'))  # Wynik: 65 (kod ASCII dla 'A')
+print(ord('Z'))  # Wynik: 90 (kod ASCII dla 'Z')
+print(ord('ą'))  # Wynik: 261 (kod Unicode dla 'ą')
+
+print(chr(97))  # Wynik: 'a' (znak dla kodu ASCII 97)
+print(chr(65))  # Wynik: 'A' (znak dla kodu ASCII 65)
+print(chr(90))  # Wynik: 'Z' (znak dla kodu ASCII 90)
+print(chr(261))  # Wynik: 'ą' (znak dla kodu Unicode 261)
