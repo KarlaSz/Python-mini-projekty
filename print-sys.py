e448ec22ad8367b88d504b65b816cdc3fdcc0ecd
Karolina Sz <128708615+KarlaSz@users.noreply.github.com>
2024-11-16 20:34:04 +0100

    Create print-sys.py

---
+++ print-sys.py
@@ -0,0 +1,39 @@
+#print
+
+#argument sep umozliwia okreslenie co byc miedzy obiektami
+print(1, 2, 5, 7, sep='-/-')
+
+#argument end domyslnie jest ze ustawiony od nowej lini dlatego print zawsze jest od nowej lini end='\n'
+print(1, 2, 5, 7, end='\n')
+print(1, 2, 5, 7, end='')
+print(1, 2, 5, 7, end='')
+
+#miejsce do ktorego wypisze sie tekst, domyslnie to sys.stdout - czyli konsola
+print(1, 2, 5, 7, file='sys.stdout')
+
+
+#flush okresla czy tekst ma byc splukany, domyslnie na Fals
+print(1, 2, 5, 7, flush=True)
+
+# import sys
+# print("Wersja Pythona:", sys.version)
+# print("System operacyjny:", sys.platform)
+# print("Interpreter znajduje się pod:", sys.executable)
+
+#Moduł sys to interfejs do wnętrza Pythona i jego środowiska uruchomieniowego. Przydaje się w sytuacjach, gdy potrzebujesz precyzyjnej kontroli nad programem lub jego interakcjami z użytkownikiem i systemem operacyjnym.
+#sys to wbudowany moduł w Pythonie, który dostarcza funkcje i zmienne do interakcji z interpreterem Pythona.
+
+#Interpreter Pythona to program, który wykonuje kod napisany w języku Python, linia po linii, tłumacząc go na instrukcje zrozumiałe dla komputera. W przeciwieństwie do języków kompilowanych (jak C czy Java), Python jest językiem interpretowanym, co oznacza, że kod nie jest kompilowany w całości przed wykonaniem, lecz interpretowany w czasie rzeczywistym.
+
+
+###co sie dzieje z kodem
+
+# Oto diagram ilustrujący proces działania Python Virtual Machine (PVM) na przykładzie:
+
+# Kod źródłowy jest pisany przez programistę w Pythonie.
+# Interpreter Pythona odbiera kod źródłowy i analizuje go.
+# Kod jest kompilowany do formatu bajtowego (bytecode), co jest etapem pośrednim.
+# Kod bajtowy może być zapisany w pliku .pyc w folderze __pycache__.
+# PVM wczytuje kod bajtowy do pamięci.
+# PVM interpretuje instrukcje bajtowe krok po kroku.
+# Instrukcje są wykonywane, a wyniki (np. dane wyjściowe) są wyświetlane.
