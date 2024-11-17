f17458f7ec1d7f4a9d8f66c4cbc321a3d47f9c53
KarlaSz <szymaszkiewiczk@gmail.com>
2024-11-17 20:28:41 +0100

    name program list comprehension

--- llist-comprehension-1-line.py
+++ llist-comprehension-1-line.py
@@ -1,4 +1,4 @@
-#list comprehension - jesto sposob skladni pisania kodu dla list
+#list comprehension - jesto sposob skladni pisania kodu dla list, set, tez dict
 #to kod w 1 linijce, alternatywa dla petli for
 
 #zadanie Cyfyry
@@ -17,3 +17,29 @@ print(new)
 # II sposob
 new = [digit ** 2 for digit in digits]
 print(new)
+
+
+#zadanie 
+# #Imiona damskie sprawdz lub meskie, podaj plec przy imieniu. wyjatek to Nel, Kuba i Beatrycze
+
+name = 'Karolina'
+## 1 sposob
+gender = 'female' if name[-1] == 'a' else 'male'
+
+print(name, gender)
+
+## 2 sposob
+
+for letter in name:
+    if name[-1] == 'a':
+        print('płeć: kobieta')
+        break
+    else:
+        print('płeć: facet')
+        break
+print('KONIEC PROGRAMU')
+ 
+##odpalaie kodu z terminala VSC to: shit + ctrl + p i wpisac Python: Select Interpreter, a pozniej na polu uruchomic prawym myszy lub zaznaczony tekst shift + enter
+
+##odpalanie kodu w Pycharm to run lub shift + f10
+
