5f08a83ae82e99b670d62ef2128c2e3d92fbb4b6
Karolina Sz <128708615+KarlaSz@users.noreply.github.com>
2024-12-27 17:55:57 +0100

    Create query.py

---
+++ sqlalchemy/query.py
@@ -0,0 +1,13 @@
+import db
+from sqlalchemy.orm import sessionmaker
+
+#all data
+for s in session.query(db.transactions).all():
+    print(s.transaction_id,s.price)
+    
+print("*"*20)
+print("Transactions with price over 40: ")
+
+#selected data
+for s in session.query(db.transactions).filter(db.transactions.price>40):
+    print(s.transaction_id,s.price)
