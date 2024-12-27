886f32ab5019bed2f183bd77b482c90a004649f8
Karolina Sz <128708615+KarlaSz@users.noreply.github.com>
2024-12-27 18:02:03 +0100

    Create main.py

---
+++ turtle/main.py
@@ -0,0 +1,21 @@
+import turtle
+
+t = turtle.Turtle()
+t.color('red')
+
+turtle.bgcolor('black')
+
+t.begin_fill()
+
+t.forward(100)
+t.right(90)
+t.forward(100)
+t.right(90)
+t.forward(100)
+t.right(90)
+t.forward(100)
+t.right(90)
+
+t.end_fill()
+
+turtle.exitonclick()
